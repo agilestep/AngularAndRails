bf870c4789f2c01d4f6659cfb68220225e35a91b
Serghei <agilestep@yahoo.com>
2015-01-27 20:36:43 +0200

    Fixing assets problem

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Recenta
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+      config.assets.initialize_on_precompile = false
 
       config.assets.paths << Rails.root.join("vendor","assets","bower_components")
       config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
